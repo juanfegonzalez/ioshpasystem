282b88170adc481f0bf07b3007d1d6e729b1b4c9
sergio garcia <sergio@flykube.com>
2024-11-17 20:10:35 +0100

    añadido items en datacore

---
+++ HPASystem-IOS/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  HPASystem-IOS
+//
+//  Created by Sergio Garcia martinez on 17/11/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
